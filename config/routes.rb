8632fbdda90160954450426e433f66a4b466f727
illumination226 <illuminationdevelopment@gmail.com>
2010-12-09 21:48:41 -0700

    more commit

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ HelloWorld::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :controller => "home"
+  root :controller => 'home', :action => 'index'
 
   # See how all your routes lay out with "rake routes"
 
