25f340464f34116c03d18239f937f2873131e9c5
illumination226 <illuminationdevelopment@gmail.com>
2010-12-09 21:43:59 -0700

    more commit

--- config/routes.rb
+++ config/routes.rb
@@ -58,9 +58,8 @@ HelloWorld::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+   map.root :controller => "home"
 
   # See how all your routes lay out with "rake routes"
 
