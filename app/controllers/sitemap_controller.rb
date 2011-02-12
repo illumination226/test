96f505df90f34cbd15811338297717f7c0f0c04a
illumination226 <illuminationdevelopment@gmail.com>
2011-02-12 01:04:15 -0700

    more commit

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -4,9 +4,9 @@ class SitemapController < ApplicationController
 
   def sitemap
     @posts = Post.find(:all, :order => "updated_at DESC", :limit => 50000)
-    respond_to do |format|
-      format.xml
-    end  
+    headers["Content-Type"] = "text/xml"
+    # set last modified header to the date of the latest entry.
+    headers["Last-Modified"] = @posts[0].updated_at.httpdate
   end
 
 end
